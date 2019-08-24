6b57a44a64bc9b728438328f53db4beb4f57c784
Arul Murugan <47811187+arulhub@users.noreply.github.com>
2019-08-24 11:25:26 +0000

    tester

---
+++ C#/new 2/LocalStore/LocalStore/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LocalStore.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ C#/new 2/LocalStore/LocalStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LocalStore.WebApiApplication" Language="C#" %>
