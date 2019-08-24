21572dc96ba01f2790b369f8da36e93c4d481386
codeprasi <33648587+codeprasi@users.noreply.github.com>
2019-08-24 09:36:09 +0000

    data fuzy

---
+++ C#/LocalStore/LocalStore/Controllers/FuzzyController.cs
@@ -0,0 +1,47 @@
+﻿//using NinjaNye.SearchExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LocalStore.Controllers
+{
+    public class FuzzyController : ApiController
+    {
+        // GET: api/Fuzzy
+        public IEnumerable<string> Get()
+        {
+            return null;
+        }
+
+        // GET: api/Fuzzy/5
+        public List<tblStoreInfo> Get(int searchId)
+        {
+            List<tblStoreInfo> stores = null;
+            using (LocalStoreEntities1 entity = new LocalStoreEntities1())
+            {
+                //stores = entity.tblStoreInfoes.Search(s => s.StoreName,
+                //                                           s => s.SearchTags).ContainingAll(searchQuery).ToList();
+
+            }
+            return stores;
+        }
+
+        // POST: api/Fuzzy
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Fuzzy/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Fuzzy/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ C#/LocalStore/LocalStore/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NinjaNye.SearchExtensions;
+
+namespace LocalStore.Controllers
+{
+    public class SearchController : ApiController
+    {
+         
+        public List<tblStoreInfo> Get(string searchQuery)
+        {
+            List<tblStoreInfo> stores = null;
+            using (LocalStoreEntities1 entity = new LocalStoreEntities1())
+            {
+                stores = entity.tblStoreInfoes.Search(s => s.StoreName,
+                                                           s => s.SearchTags).ContainingAll(searchQuery).ToList();
+
+            }
+            return stores;
+        }
+    }
+}
