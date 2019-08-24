bb446501a1b45e09c58e3049a4e3ca64d6737808
codeprasi <33648587+codeprasi@users.noreply.github.com>
2019-08-24 11:35:03 +0000

    newwst

---
+++ C#/new 3/LocalStore/LocalStore/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LocalStore.Controllers
+{
+    public class ProductController : ApiController
+    {
+        // GET: api/Product
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Product/5
+        public List<tblProductInfo> Get(string StoreAuthId)
+        {
+            List<tblProductInfo> products = null;
+            using (LocalStoreEntities1 entity = new LocalStoreEntities1())
+            {
+                products = entity.tblProductInfoes.Where(p => p.StoreAuthId== StoreAuthId).ToList();
+            }
+            return products;
+        }
+
+        // POST: api/Product
+        public void Post([FromBody]tblProductInfo value)
+        {
+            using (LocalStoreEntities1 entity = new LocalStoreEntities1())
+            {
+                entity.tblProductInfoes.Add(value);
+                entity.SaveChanges();
+            }
+        }
+
+        // PUT: api/Product/5
+        public void Put(string StoreAuthId, [FromBody]tblProductInfo value)
+        {
+
+            using (LocalStoreEntities1 entity = new LocalStoreEntities1())
+            {
+                var products = entity.tblProductInfoes.Where(p => p.StoreAuthId == StoreAuthId).ToList<tblProductInfo>();
+                if (products !=  null)
+                {
+                    //var product = products.Where(z => z.ProductId == value.ProductId).FirstOrDefault();
+                    //if (product)
+
+
+                }
+                entity.SaveChanges();
+            }
+        }
+
+        // DELETE: api/Product/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ C#/new 3/LocalStore/LocalStore/Controllers/StoreRegistrationController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace LocalStore.Controllers
+{
+    
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    public class StoreRegistrationController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<tblStoreInfo> Get()
+        {
+            LocalStoreEntities1 entity = new LocalStoreEntities1();
+            var stores = (from e in entity.tblStoreInfoes
+                          select e).ToList();
+
+            return stores;
+        }
+
+        // GET api/values/5
+        public tblStoreInfo Get(string StoreAuthId)
+        {
+            tblStoreInfo store = null;
+            using (LocalStoreEntities1 entity = new LocalStoreEntities1())
+            {
+                store = entity.tblStoreInfoes.Where(p => p.StoreAuthId == StoreAuthId).FirstOrDefault();
+            }
+            return store;
+        }
+
+        // POST api/values
+        public string Post([FromBody]tblStoreInfo value)
+        {
+            value.StoreAuthId = value.StoreName.Replace(" ",string.Empty) + "@LocalStore.com"  ;
+            using (LocalStoreEntities1 entity = new LocalStoreEntities1())
+            {
+               entity.tblStoreInfoes.Add(value);
+                entity.SaveChanges();
+            }
+            return value.StoreAuthId;
+        }
+
+        // PUT api/values/5
+        public void Put(string StoreAuthId, [FromBody]tblStoreInfo value)
+        {
+            using (LocalStoreEntities1 entity = new LocalStoreEntities1())
+            {
+                var oldStore = entity.tblStoreInfoes.Where(p => p.StoreAuthId == StoreAuthId).FirstOrDefault();
+                if (oldStore != null)
+                {
+                    oldStore.StoreName = value.StoreName;
+                    oldStore.StoreAddress = value.StoreAddress;
+                    oldStore.Pincode = value.Pincode;
+                    oldStore.SearchTags = value.SearchTags;
+                    entity.SaveChanges();
+                }
+                
+            }
+        }
+
+        // DELETE api/values/5
+        public void Delete(string StoreAuthId)
+        {
+            using (LocalStoreEntities1 entity = new LocalStoreEntities1())
+            {
+                var oldStore = entity.tblStoreInfoes.Where(p => p.StoreAuthId == StoreAuthId).FirstOrDefault();
+                entity.tblStoreInfoes.Remove(oldStore);
+                entity.SaveChanges();
+            }
+        }
+    }
+}
